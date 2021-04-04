8f0411a65233ed8ae864e25fc14323adf0c82e75
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-04-03 22:48:56 -0400

    Add project files.

---
+++ RedBadgeMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedBadgeMVCProject.Startup))]
+namespace RedBadgeMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
