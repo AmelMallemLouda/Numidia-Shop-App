44e7c7b01533929dcd45d9e30112ec12b3083d20
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-04-10 15:40:33 -0400

    data

---
+++ RedBadgeMVC.Data/Category.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadgeMVC.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+
+        [Display(Name = "Clothing Category ")]
+        public int ClothingId { get; set; }
+
+        [ForeignKey("ClothingId")]
+        public virtual Clothing Clothing { get; set; }
+
+
+        [Display(Name = "Home & Kitchen Category ")]
+        public int HomeId { get; set; }
+
+        [ForeignKey("HomeId")]
+        public virtual HomeKitchen Home { get; set; }
+
+
+        [Display(Name = "Beauty & Health Category ")]
+        public int BeautyHealthId { get; set; }
+
+        [ForeignKey("BeautyHealthId")]
+        public virtual BeautyHealth BeautyHealth { get; set; }
+
+       //public virtual List<Item> Items { get; set; } = new List<Item>();
+
+
+
+
+
+    }
+}
---
+++ RedBadgeMVC.Data/HomeKitchen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadgeMVC.Data
+{
+    public class HomeKitchen
+    {
+        [Key]
+        public int HomeKitchenId { get; set; }
+
+        [Display(Name = "Home & Kitchen")]
+        public string HomeKitchenName { get; set; }
+        //public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
